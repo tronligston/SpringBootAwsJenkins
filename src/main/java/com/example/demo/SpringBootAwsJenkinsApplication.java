5d1c635ab54a711567492572963c355404f13f74
lmehdi <tronligston@gmail.com>
2019-12-09 12:18:43 +0000

    first commit

---
+++ src/main/java/com/example/demo/SpringBootAwsJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsJenkinsApplication.class, args);
+	}
+
+}
